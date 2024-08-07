444a4b565ccf535a31d731eeaf5dfa797690621b
devinisaperson <devinhhuang@gmail.com>
2024-08-07 18:59:42 -0400

    Create Day3.java

---
+++ Day3.java
@@ -0,0 +1,7 @@
+public class Day3 {
+    public static void main(String[] args) {
+        int thisCanChange = 42;
+        final String THIS_CANNOT_CHANGE = "The answer to the Great Question of Life, the Universe and Everything... is... ";
+        System.out.print(THIS_CANNOT_CHANGE + thisCanChange);
+    }
+}
