cffca16d5a59b93e4e87dadfb0d1dd86cfd216bd
devinisaperson <devinhhuang@gmail.com>
2024-08-07 19:03:33 -0400

    Update Day3.java

--- Day3.java
+++ Day3.java
@@ -3,5 +3,15 @@ public class Day3 {
         int thisCanChange = 42;
         final String THIS_CANNOT_CHANGE = "The answer to the Great Question of Life, the Universe and Everything... is... ";
         System.out.print(THIS_CANNOT_CHANGE + thisCanChange);
+
+        try {
+            thisCanChange = -42;
+            System.out.println("This code has run without issue, as expected.");
+        } catch (Exception e) {
+            System.out.println("Something has gone very wrong.");
+        }
+
+        //THIS_CANNOT_CHANGE = "You've met with a terrible fate, haven't you?";
+        //The above code will refuse to even compile
     }
 }
